cd11b4b26c9d134b104fdbc9d03aa6556b7f126a
Dave Gumba <dave_gumba@Yahoo.com>
2020-01-01 23:13:43 -0500

    working on the css for the size settings

--- src/Helpers/generate_json.py
+++ src/Helpers/generate_json.py
@@ -12,18 +12,22 @@ with open(filename) as f:
 
 id = 0
 for word_raw in content:
-    word_raw = word_raw.rstrip()
-    word_values = word_raw.split('|')
+    if not word_raw.startswith('#'):
+        word_raw = word_raw.rstrip()
+        word_values = word_raw.split('|')
 
-    data['deutsch'] = word_values[0]
-    data['english'] = word_values[1]
-    data['type'] = word_values[2]
-    data['translationId'] = id
-    id += 1
+        data['deutsch'] = word_values[0]
+        data['english'] = word_values[1]
+        data['type'] = word_values[2]
+        data['category'] = word_values[3] if len(word_values) > 3 else 'none'
+        data['translationId'] = id
+        id += 1
 
-    # necessary encoding is for umlauts
-    json_data = json.dumps(data, ensure_ascii=False).encode('utf8')
-    json_words.append(json_data.decode())
+        # necessary encoding is for umlauts
+        json_data = json.dumps(data, ensure_ascii=False).encode('utf8')
+        json_words.append(json_data.decode())
+
+# print(json_words)
 
 with open(json_filename, 'w', encoding='utf8') as json_file:
     json.dump(json_words, json_file, ensure_ascii=False)
