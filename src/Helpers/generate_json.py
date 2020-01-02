d39cbaf6a2cc08a8195c001850b7e3c378f08237
Dave Gumba <dave_gumba@Yahoo.com>
2020-01-01 21:26:58 -0500

    json generates. fixed an error where size would output something not chosen by the user

---
+++ src/Helpers/generate_json.py
@@ -0,0 +1,29 @@
+# https://stackoverflow.com/questions/18337407/saving-utf-8-texts-in-json-dumps-as-utf8-not-as-u-escape-sequence
+
+import json
+
+json_words = [] # the main output
+
+data = {}
+filename = 'words.txt'
+json_filename = 'words.json'
+with open(filename) as f:
+    content = f.readlines()
+
+id = 0
+for word_raw in content:
+    word_raw = word_raw.rstrip()
+    word_values = word_raw.split('|')
+
+    data['deutsch'] = word_values[0]
+    data['english'] = word_values[1]
+    data['type'] = word_values[2]
+    data['translationId'] = id
+    id += 1
+
+    # necessary encoding is for umlauts
+    json_data = json.dumps(data, ensure_ascii=False).encode('utf8')
+    json_words.append(json_data.decode())
+
+with open(json_filename, 'w', encoding='utf8') as json_file:
+    json.dump(json_words, json_file, ensure_ascii=False)
